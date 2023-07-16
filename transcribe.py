8842a6095c7cf5753517271001f7b6ee305ca945
Wheely <alexchapin@hotmail.fr>
2023-07-16 19:19:13 +0200

    First version of the GUI + pre visualisation

--- transcribe.py
+++ transcribe.py
@@ -7,6 +7,7 @@ import os
 import pickle
 import random
 import PySimpleGUI as sg
+import matplotlib.font_manager as fm
 
 def extract_word_timestamps(audio_path : str, whisper_size : str ="large", language : str = "French"):
     """Function that takes the path of an audio to extract the transcription
@@ -138,16 +139,20 @@ def draw_text_on_image(
     # Convert the PIL Image back to OpenCV format
     return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
-def edit_video():
-    # TODO : setup an argparse
-    # Parameters 
+def export_video():
+    pass
+
+if __name__ == '__main__':
+    # TODO : setup an argparse system
+    ################################
+    #      Prepare parameters      #  
+    ################################
     video_path = "short.mp4"
     temp_path = "temp.mp4"
     final_path = "final.mp4"
     transcript_path = "word_timestamps.txt"
     filename, ext = os.path.splitext(video_path)
     audio_path = f"{filename}.wav"
-    #font_path = "C:\\Users\\alexc\\AppData\\Local\\Microsoft\\Windows\\Fonts\\built titling bd.ttf"
     font_path = "C:\\Users\\alexc\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Montserrat-ExtraBold.otf"
     font_size = 70
     text_color = (255, 255, 255)
@@ -162,7 +167,7 @@ def edit_video():
     clip = VideoFileClip(video_path)
     clip.audio.write_audiofile(audio_path)
 
-    # TODO : load text
+    # TODO : load the text properly
     if transcript_path == None:
         temp_word_timestamp = extract_word_timestamps(audio_path)
         line_1, line_2, word_timestamps  = process_words(temp_word_timestamp)
@@ -172,24 +177,84 @@ def edit_video():
         with open(transcript_path, "rb") as f:
             temp_word_timestamp = pickle.load(f)
         line_1, line_2, word_timestamps = process_words(temp_word_timestamp)
-        
-    ################################
-    #   Draw text back to video    #  
-    ################################
-    cap = cv2.VideoCapture(video_path)
 
+    # TODO : add progress bar while simulating :
+    # https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Progress_Meter_Simulated.py
+    
+    ################################
+    #   Preprare the windows GUI   #  
+    ################################
+    ### Get the list of installed font on the computer
+    all_font_path = fm.findSystemFonts()
+    fonts = []
+    for path in all_font_path:
+        fonts.append(path.split("\\")[-1].split(".")[0])
+    fonts.sort()
+
+    # List all available fonts
+    sg.theme('Black')
+    
+    cap = cv2.VideoCapture(video_path)
+    
     # Define the output video codec and create a VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     fps = cap.get(cv2.CAP_PROP_FPS)
+    num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
     out = cv2.VideoWriter(temp_path, fourcc, fps, (frame_width, frame_height))
 
-    current_frame_id = 0
-    font = ImageFont.truetype(font_path, font_size)
-    highlight_font = ImageFont.truetype(font_path, font_size+4)
+    ### Prepare the layout 
+    layout = [
+            [sg.Column(
+                [[sg.Text('Exemple de texte',
+                    size=(20, 1),
+                    click_submits=True,
+                    relief=sg.RELIEF_GROOVE,
+                    font='Courier` 25',
+                    text_color='#000000',
+                    background_color='white',
+                    justification='center',
+                    pad=(5, 3),
+                    key='-text-',
+                    tooltip='Exemple du texte qui sera affiché dans la vidéo',
+                    )],
+                [sg.Listbox(fonts, size=(30, 20), change_submits=True, key='-list-')],
 
-    old_text = get_current_text(line_1, 0) + ' ' + get_current_text(line_2, 0)
+                [sg.CB('Gras', key='-bold-', change_submits=True),
+                    sg.CB('Italique', key='-italics-', change_submits=True),
+                    sg.CB('Soulignement', key='-underline-', change_submits=True)],
+                [sg.Input(key='-font_size-', change_submits=True, size=(10, 5)),
+                    sg.Text('Taille du texte (doit être un nombre !)')],
+                [sg.Button('Exporter')]
+                ], 
+                element_justification='c'
+            ),
+            sg.Column(
+                [
+                    [sg.Image(key='-image-')],
+                    [sg.Slider(range=(0, num_frames), size=(30, 10), orientation='h', key='-vid_slider-', change_submits=True)],
+                ], 
+                element_justification='c'
+            )]]
+
+    window = sg.Window('Video transcripteur automatique', layout, size=(1280, 720))
+
+    ###Locate the elements we'll be updating. Does the search only 1 time
+    image_elem = window['-image-']
+    slider_elem = window['-vid_slider-']
+    timeout = 1000//fps  
+    text_elem = window['-text-']
+
+    
+    ################################
+    #   Draw text back to video    #  
+    ################################
+    """cap = cv2.VideoCapture(video_path)
+
+
+    current_frame_id = 0
 
     # Process each frame of the video
     while cap.isOpened():
@@ -230,44 +295,75 @@ def edit_video():
     final_clip = VideoFileClip(temp_path)
     audio_clip = AudioFileClip(audio_path)
     final_clip = final_clip.set_audio(audio_clip)
-    final_clip.write_videofile(final_path)
+    final_clip.write_videofile(final_path)"""
 
-if __name__ == '__main__':
-    #edit_video()
+    cur_frame = 0
+    font = ImageFont.truetype(font_path, font_size)
+    highlight_font = ImageFont.truetype(font_path, font_size+4)
 
-    # TODO : use https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Font_String.py 
-    # to handle fonts
-    fonts = sg.Text.fonts_installed_list()
-
-    sg.theme('Black')
-
-    layout = [[sg.Text('My Text Element',
-                    size=(20, 1),
-                    click_submits=True,
-                    relief=sg.RELIEF_GROOVE,
-                    font='Courier` 25',
-                    text_color='#FF0000',
-                    background_color='white',
-                    justification='center',
-                    pad=(5, 3),
-                    key='-text-',
-                    tooltip='This is a text element',
-                    )],
-            [sg.Listbox(fonts, size=(30, 20), change_submits=True, key='-list-')],
-            [sg.Input(key='-in-')],
-            [sg.Button('Read', bind_return_key=True), sg.Exit()]]
-
-    window = sg.Window('My new window', layout)
-
-    while True:     # Event Loop
+    old_text = get_current_text(line_1, 0) + ' ' + get_current_text(line_2, 0)
+    ### Open the video
+    while True:     
+        ### Read the events
         event, values = window.read()
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
-        text_elem = window['-text-']
-        print(event, values)
-        if values['-in-'] != '':
-            text_elem.update(font=values['-in-'])
+
+        ret, frame = cap.read()
+
+        ### Update frame with slider
+        if int(values['-vid_slider-']) != cur_frame-1:
+            cur_frame = int(values['-vid_slider-'])
+            cap.set(cv2.CAP_PROP_POS_FRAMES, cur_frame)
+        slider_elem.update(cur_frame)
+        
+        # Convert the frame to PIL Image
+        pil_image = Image.fromarray(cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2RGB))
+
+        # Draw the text and prompt on the PIL Image
+        draw = ImageDraw.Draw(pil_image)
+
+        current_time = cur_frame / fps
+
+        frame_text_1 = draw_text_on_image(
+            line_1, draw, current_time, font, highlight_font,
+            frame_width, word_timestamps, 50, pil_image)
+        frame_text_2 = draw_text_on_image(
+            line_2, draw, current_time, font, highlight_font,
+            frame_width, word_timestamps, 150, frame_text_1, to_pil=False)
+
+        out.write(frame_text_2)
+
+        # Display the frame (optional)
+        imS = cv2.resize(frame_text_2, (360, 640))
+
+        # TODO : try to use PIl as done in existing function
+        imgbytes = cv2.imencode('.png', imS)[1].tobytes()  
+        image_elem.update(data=imgbytes)
+        
+        ### Handle font
+        tuple_font = []
+        if values['-list-']:
+            tuple_font.append(values['-list-'][0])
+            for path in all_font_path:
+                if values['-list-'][0] == path.split("\\")[-1].split(".")[0]:
+                    font_path = path
+        else: 
+            tuple_font.append('Helvetica')
+        if values['-font_size-']:
+            font_size = int(values['-font_size-'])
+            tuple_font.append(values['-font_size-'])
         else:
-            text_elem.update(font=(values['-list-'][0], 25))
+            font_size = 20
+            tuple_font.append("20")
+        if values['-bold-']:
+            tuple_font.append('bold')
+        if values['-italics-']:
+            tuple_font.append('italic')
+        if values['-underline-']:
+            tuple_font.append('underline')
+        font = ImageFont.truetype(font_path, font_size)
+        highlight_font = ImageFont.truetype(font_path, font_size+4)
+        #text_elem.update(font=tuple(tuple_font))
     window.close()
 
